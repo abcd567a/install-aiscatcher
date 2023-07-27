e80c2420a60a084ea280bc4ffbd35c632294bc3d
abcd567a <abcd567@hotmail.com>
2023-07-27 02:28:27 -0400

    Update install-aiscatcher.sh

--- install-aiscatcher.sh
+++ install-aiscatcher.sh
@@ -8,7 +8,8 @@ sudo apt install -y g++
 sudo apt install -y cmake
 sudo apt install -y pkg-config
 sudo apt install -y librtlsdr-dev
-sudo apt install -y whiptail
+##sudo apt install -y whiptail
+sudo apt install -y dialog
 sudo apt install -y libpq-dev
 
 INSTALL_FOLDER=/usr/share/aiscatcher
@@ -38,7 +39,8 @@ sudo chmod 644 ${CONFIG_FILE}
 
 
 if [[ -f "${INSTALL_FOLDER}/aiscatcher.conf" ]]; then
-   CHOICE=$(sudo whiptail --title "CONFIG" --menu "An existing config file 'aiscatcher.conf' found. What you want to do with it?" 20 60 5 \
+   ##CHOICE=$(sudo whiptail --title "CONFIG" --menu "An existing config file 'aiscatcher.conf' found. What you want to do with it?" 20 60 5 \
+   CHOICE=$(sudo dialog --title "CONFIG" --menu "An existing config file 'aiscatcher.conf' found. What you want to do with it?" 20 60 5 \
    "1" "KEEP existing config file \"aiscatcher.conf\" " \
    "2" "REPLACE existing config file by default config file" 3>&1 1>&2 2>&3);
    if [[ ${CHOICE} == "2" ]]; then
